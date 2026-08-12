da9712846232966fe5a3609e7601cfd0d4f03c89
suino <sampaiov917@gmail.com>
2026-08-11 23:40:24 -0300

    Segundo commit

---
+++ src/main/java/com/grupo/tfc/conectapro/ConectaproApplication.java
@@ -0,0 +1,13 @@
+package com.grupo.tfc.conectapro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectaproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectaproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo/tfc/conectapro/ConectaproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo.tfc.conectapro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectaproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
